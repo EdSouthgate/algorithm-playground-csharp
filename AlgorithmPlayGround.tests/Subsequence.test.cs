3e5380e26458a2c3834686dd359c5b8ee6b3b543
Edmund Southgate <edsouthgate@me.com>
2022-12-18 20:17:31 +0000

    added issubsequence algorithm

---
+++ AlgorithmPlayGround.tests/Subsequence.test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+using AlgorithmPlayground;
+
+namespace AlgorithmPlaygroundTests
+{
+    public class SubsequenceTests
+    {
+        [Fact]
+        public void ShouldReturnTrueIfSEqualsT()
+        {
+            // Arrange 
+            string input1 = "a";
+            string input2 = "a";
+            // Act 
+            Boolean result = Subsequence.IsSubsequence(input1, input2);
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldReturnFalseIfSDoesNotEqualT()
+        {
+            // Arrange 
+            string input1 = "a";
+            string input2 = "b";
+            // Act 
+            Boolean result = Subsequence.IsSubsequence(input1, input2);
+            // Assert 
+            Assert.False(result);
+        }
+
+
+        [Fact]
+        public void ShouldReturnTrueIfTIsASubSequenceOfS()
+        {
+            // Arrange 
+            string input1 = "abc";
+            string input2 = "aghblpc";
+            // Act 
+            Boolean result = Subsequence.IsSubsequence(input1, input2);
+            // Assert 
+            Assert.True(result);
+        }
+    }
+}
---
+++ AlgorithmPlayground/Subsequence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPlayground
+{
+    public class Subsequence
+    {
+        public static Boolean IsSubsequence(string s, string t)
+        {
+            for(int i = 0; i < s.Length; i++)
+            {
+                char sLetter = s[i];
+                int currIndex = i;
+                bool letterFound = false;
+                for(int j = currIndex; j < t.Length; j++)
+                {
+                    if(sLetter == t[j])
+                    {
+                        letterFound = true;
+                        break;
+                    }
+                }
+                if(!letterFound)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
